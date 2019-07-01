2c0f86ea8c73052cdbcc47c5069ea8a2951e4f68
Daniel Duran Schutz <danissch@gmail.com>
2019-07-01 11:59:14 -0500

    Initial Commit

---
+++ farmatodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  farmatodo
+//
+//  Created by Daniel Duran Schutz on 7/1/19.
+//  Copyright © 2019 OsSource. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
